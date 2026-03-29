516b4c9329b2c3fd85dc408e281942ed46019721
Adnan Tahir <a.tahir2@uva.nl>
2026-03-29 22:41:19 +0200

    fix: add basePath for subpath hosting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.NODE_ENV === 'production' ? '/grisc-saq' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/grisc-sa' : '',
 };
 
 module.exports = nextConfig;
